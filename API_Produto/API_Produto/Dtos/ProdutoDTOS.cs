77aa76470b8dfd13a7df962e87e06e67fb20fe57
Paulo Henrique <paulo.souza@estudante.ifro.edu.br>
2024-09-23 15:56:42 -0400

    Commit API-produto

---
+++ API_Produto/API_Produto/Dtos/ProdutoDTOS.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace PDS.Models
+{
+    public class ProdutoDTO
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Codigo { get; set; }
+
+        public double PrecoCompra { get; set; }
+
+        public double PrecoVenda { get; set; }
+
+        public string Quantidade { get; set; }
+
+        public string Fornecedor { get; set; }
+
+
+    }
+
+
+}
